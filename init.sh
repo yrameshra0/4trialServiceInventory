d6896a9f6d47c57e5368a5d74041fa7671a60540
Y Ramesh Rao <yrameshrao@gmail.com>
2019-01-13 04:56:25 +0100

    Systematically avoiding docker command when no service can be installed

--- init.sh
+++ init.sh
@@ -8,16 +8,21 @@ echo "$value" |
     while read -r service_name 
     do
         #Ignores the first line of the services file which is informative
+        if [ "$("$service_name" | grep -c "#")" -eq 1 ]; then
+            continue
+        fi
         service_already_installed=$(docker service ls --format "{{.Name}}" | grep -c "$service_name")
-        if [ $count -ge 1 ] && [ "$service_already_installed" -eq 0 ]; then 
+        if [ "$service_already_installed" -eq 0 ]; then 
             printf "    %s:\n" "$service_name">>services.yml
             printf "      image: scratch:latest\n">>services.yml   
+            count=$((count+1))
         fi 
-        count=$((count+1))
     done
 
-    printf "\nInstalling services to prod \n"
-    docker stack deploy -c services.yml prod
-    printf "\nInstalling services to test \n"
-    docker stack deploy -c services.yml test
+    if [ $count -ge 1 ]; then
+        printf "\nInstalling services to prod \n"
+        docker stack deploy -c services.yml prod
+        printf "\nInstalling services to test \n"
+        docker stack deploy -c services.yml test
+    fi
 }
